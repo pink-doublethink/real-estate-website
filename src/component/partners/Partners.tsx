78e686bb201aaff59cd97ad844141d6aabcacf26
Alexey Ovsyannikov <soliterco@gmail.com>
2023-02-21 13:06:34 +0300

    add 8 components

---
+++ src/component/partners/Partners.tsx
@@ -0,0 +1,43 @@
+import './partners.css'
+
+function Partners() {
+  return (
+    <section className="partners">
+                    <div className="partners__container container">
+                        <div className="partners__body">
+                            <h2 className="partners__subtitle subtitle">С кем мы работаем</h2>
+                            <div className="partners__text">С нами работают самые крупные арендаторы и ритейлы Челябинска а также ведущие
+                                банки России</div>
+                            <div className="partners__slider">
+                                <div className="partners__image"> <img src="template/images/p01.webp" alt="" /></div>
+                                <div className="partners__image"> <img src="template/images/p01.webp" alt="" /></div>
+                                <div className="partners__image"> <img src="template/images/p01.webp" alt="" /></div>
+                                <div className="partners__image"> <img src="template/images/p01.webp" alt="" /></div>
+                                <div className="partners__image"> <img src="template/images/p01.webp" alt="" /></div>
+                                <div className="partners__image"> <img src="template/images/p01.webp" alt="" /></div>
+                                <div className="partners__image"> <img src="template/images/p01.webp" alt="" /></div>
+                                <div className="partners__image"> <img src="template/images/p01.webp" alt="" /></div>
+                                <div className="partners__image"> <img src="template/images/p01.webp" alt="" /></div>
+                                <div className="partners__image"> <img src="template/images/p01.webp" alt="" /></div>
+                                <div className="partners__image"> <img src="template/images/p01.webp" alt="" /></div>
+                                <div className="partners__image"> <img src="template/images/p01.webp" alt="" /></div>
+                                <div className="partners__image"> <img src="template/images/p01.webp" alt="" /></div>
+                                <div className="partners__image"> <img src="template/images/p01.webp" alt="" /></div>
+                                <div className="partners__image"> <img src="template/images/p01.webp" alt="" /></div>
+                                <div className="partners__image"> <img src="template/images/p01.webp" alt="" /></div>
+                                <div className="partners__image"> <img src="template/images/p01.webp" alt="" /></div>
+                                <div className="partners__image"> <img src="template/images/p01.webp" alt="" /></div>
+                                <div className="partners__image"> <img src="template/images/p01.webp" alt="" /></div>
+                                <div className="partners__image"> <img src="template/images/p01.webp" alt="" /></div>
+                                <div className="partners__image"> <img src="template/images/p01.webp" alt="" /></div>
+                                <div className="partners__image"> <img src="template/images/p01.webp" alt="" /></div>
+                                <div className="partners__image"> <img src="template/images/p01.webp" alt="" /></div>
+                                <div className="partners__image"> <img src="template/images/p01.webp" alt="" /></div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+  )
+}
+
+export default Partners
